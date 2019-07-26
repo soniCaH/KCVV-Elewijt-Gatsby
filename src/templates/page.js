9751f712ef7075d802529d245591c00c4ee020bd
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2019-07-26 10:42:18 +0200

    Smaller clug logo images.

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../layouts/index'
 import SEO from '../components/seo'
 import Img from 'gatsby-image'
