cb23ffccfd14a528c38c4f0e1430ed9eed98cc38
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2019-07-28 09:13:18 +0200

    Matches unstyled.

--- src/components/team-calendar-matches.js
+++ src/components/team-calendar-matches.js
@@ -85,32 +85,56 @@ class TeamCalendarMatches extends React.Component {
   }
 
   render() {
+    let moment = require('moment')
+    let timezone = require('moment-timezone')
+    moment.locale('nl-BE')
+
     if (this.state.loading === false && this.state.data) {
-    //   this.state.data.sort((a, b) => {
-    //     return a.position - b.position !== 0
-    //       ? a.position - b.position
-    //       : b.goalsPro - b.goalsAgainst - (a.goalsPro - a.goalsAgainst)
-    //   })
+      const elewijtMatches = this.state.data
+        .filter(
+          ({ regNumberHome, regNumberAway }) =>
+            regNumberHome === '00055' || regNumberAway === '00055'
+        )
+        .sort((a, b) =>
+          a.dateTime > b.dateTime ? 1 : b.dateTime > a.dateTime ? -1 : 0
+        )
 
       return (
         <table className="rankingTable">
           <thead>
             <tr className="rankingRow">
-              <th className="rankingRow-Position rankingRow-Integer">#</th>
-              <th className="rankingRow-Teamname">Team</th>
-              <th className="rankingRow-Matches rankingRow-Integer">M</th>
-              <th className="rankingRow-Wins rankingRow-Integer">W</th>
-              <th className="rankingRow-Draws rankingRow-Integer">D</th>
-              <th className="rankingRow-Losses rankingRow-Integer">L</th>
-              <th className="rankingRow-GoalsPro rankingRow-Integer">G+</th>
-              <th className="rankingRow-GoalsAgainst rankingRow-Integer">G-</th>
-              <th className="rankingRow-GoalsDiff rankingRow-Integer">+/-</th>
-              <th className="rankingRow-Points rankingRow-Integer">Pts</th>
+              <th className="rankingRow-Position rankingRow-Integer">Datum</th>
+              <th className="rankingRow-Teamname">Uur</th>
+              <th className="rankingRow-Matches rankingRow-Integer">
+                Thuisploeg
+              </th>
+              <th className="rankingRow-Wins rankingRow-Integer">Score</th>
+              <th className="rankingRow-Draws rankingRow-Integer">Uitploeg</th>
+              <th className="rankingRow-Losses rankingRow-Integer">Status</th>
             </tr>
           </thead>
           <tbody>
-            {this.state.data.map((result, i) => {
-              console.log(result);
+            {elewijtMatches.map((result, i) => {
+              const d = new moment(result.dateTime)
+              const date = d.format('dddd D MMMM YYYY')
+              const dateShort = d.format('DD/MM/YYYY')
+              const time = d.format('HH:mm')
+              return (
+                <tr>
+                  <td className={'display-desktop'}>{date}</td>
+                  <td className={'display-mobile'}>{dateShort}</td>
+                  <td>{time}</td>
+                  <td>{result.home}</td>
+                  <td>
+                    {typeof result.resultHome !== 'undefined' &&
+                    typeof result.resultAway !== 'undefined'
+                      ? `${result.resultHome} - ${result.resultAway}`
+                      : 'vs'}
+                  </td>
+                  <td>{result.away}</td>
+                  <td>{result.status}</td>
+                </tr>
+              )
             })}
           </tbody>
         </table>
