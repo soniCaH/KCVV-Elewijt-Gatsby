19000537a2673dec53a0163699abb3ec38ff45e8
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2019-08-20 11:38:47 +0200

    Geschiedenis KCVV Elewijt.

---
+++ src/pages/club/history.js
@@ -0,0 +1,149 @@
+import React, { Component } from 'react'
+
+import Layout from '../../layouts/index'
+
+import SEO from '../../components/seo'
+
+class HistoryPage extends Component {
+  render() {
+    return (
+      <Layout>
+        <SEO lang="nl-BE" title="Er is maar één plezante compagnie" />
+
+        <div class="timeline">
+          <div class="timeline-item">
+            <div class="timeline-icon" />
+            <div class="timeline-content">
+              <p class="timeline-content-date">1913 - 1959</p>
+              <p>
+                In 1913 werd <strong>Crossing FC Ganshoren</strong> opgericht.
+                Ganshoren is een gemeente in het noordwesten van het Brussels
+                Hoofdstedelijk Gewest. De club sloot aan bij de voetbalbond en
+                kreeg later stamnummer 451 toegekend. Bij het 25-jarig bestaan
+                in 1938 werd de club koninklijk en heette Royal Crossing FC
+                Ganshoren. In 1942, tijdens de Tweede Wereldoorlog, verscheen de
+                club in de bevorderingsreeksen, aanvankelijk de Derde Klasse,
+                vanaf 1952 de Vierde Klasse, waar de club tot 1959 speelde.
+              </p>
+            </div>
+          </div>
+
+          <div class="timeline-item">
+            <div class="timeline-icon"></div>
+            <div class="timeline-content right">
+              <p class="timeline-content-date">1959 - 1969</p>
+              <p>
+                De club veranderde in 1959 van naam en werd{' '}
+                <strong>Royal Crossing Club Molenbeek</strong> en verhuisde naar
+                Sint-Jans-Molenbeek. Datzelfde jaar promoveerde de club uit
+                Bevordering naar Derde Klasse. De club zat in de lift en won
+                drie seizoenen later zijn reeks. Zo mocht de club in 1962 voor
+                het eerst van start in Tweede Klasse. In 1969 werd de club
+                tweede in Tweede Klasse, met evenveel punten als winnaar AS
+                Oostende.
+              </p>
+            </div>
+          </div>
+
+          <div class="timeline-item">
+            <div class="timeline-icon"></div>
+            <div class="timeline-content">
+              <p class="timeline-content-date">1969 - 1983</p>
+              <p>
+                De club fusioneerde dat jaar met Royal Cercle Sportif de
+                Schaerbeek. RCS de Schaerbeek had in zijn geschiedenis
+                verschillende seizoenen in Tweede Klasse gespeeld, maar speelde
+                na de Tweede Oorlog in Derde en Vierde Klasse. In 1966 was deze
+                club opnieuw weggezakt naar Vierde en in 1968 zakte de club als
+                voorlaatste in zijn reeks uit de nationale afdelingen. De
+                fusieclub heette{' '}
+                <strong>Royal Crossing Club de Schaerbeek</strong> en ging in
+                het Crossingstadion in Josaphatpark, Schaarbeek spelen onder
+                stamnummer 55 van CS Schaerbeek. De ploeg, bijgenaamd{' '}
+                <i>de Ezels</i>, had groen-wit als kleuren. Door de net behaalde
+                tweede plaats van Crossing startte de fusieclub in 1969 in de
+                Eerste Klasse. De club speelde vier seizoenen in de hoogste
+                afdeling, maar eindigde telkens in de degradatiezone. In 1973
+                eindigde RCC de Schaerbeek uiteindelijk afgetekend laatste en
+                zakte terug naar Tweede Klasse. De club zakte nu snel weer weg.
+                Na twee seizoenen in Tweede volgde in 1975 immers alweer een
+                degradatie naar Derde en in 1980 zakte de club naar Vierde
+                Klasse. In 1983 werd de club roemloos laatste in zijn reeks, met
+                slechts 4 punten (één zege en twee gelijke spelen) in 30
+                wedstrijden en zakte weg naar Eerste Provinciale.
+              </p>
+            </div>
+          </div>
+
+          <div class="timeline-item">
+            <div class="timeline-icon"></div>
+            <div class="timeline-content right">
+              <p class="timeline-content-date">1983 - 1991</p>
+              <p>
+                De club verliet in 1983 het Crossingstadion en verhuisde naar
+                Elewijt. De nieuwe clubnaam werd{' '}
+                <strong>Royal Crossing Elewijt</strong>. De club speelde verder
+                in de hogere provinciale reeksen, maar slaagde er niet meer in
+                de nationale bevorderingsreeksen te bereiken. Crossing Elewijt
+                won in 1988 wel de Beker van Brabant. In 1991 fusioneerde club
+                uiteindelijk met een andere Elewijtse ploeg, VV Elewijt. VV
+                Elewijt speelde in de lagere provinciale reeksen en had als
+                hoogtepunt een seizoen een top-5 plaats behaald in Tweede
+                Provinciale. De fusieclub heette{' '}
+                <strong>KVV Crossing Elewijt (afgekort KCVV Elewijt)</strong>,
+                maar zakte wel weg tot in de allerlaagste provinciale
+                voetbalreeksen.
+              </p>
+            </div>
+          </div>
+
+          <div class="timeline-item">
+            <div class="timeline-icon"></div>
+            <div class="timeline-content">
+              <p class="timeline-content-date">2001 - 2018</p>
+              <p>
+                KCVV Elewijt werd in 2001 tweede in de laagste provinciale reeks
+                en dwong zo de promotie naar derde provinciale af. Het
+                daaropvolgende seizoen werden ze laatste en degradeerden ze
+                opnieuw naar 4e provinciale. Na zeven seizoenen als subtobber
+                slaagden ze er in 2009 opnieuw in om de promotie af te dwingen.
+                Ditmaal hielden ze twee seizoenen stand, waarna ze in het
+                seizoen 2011/2012 opnieuw uitkwamen in vierde provinciale. KCVV
+                speelde meteen kampioen, waarna ze erin slaagden een stabiele
+                derdeprovincialer te worden.
+              </p>
+              <p>
+                Na een paar turbulente jaren begint de club, door de
+                inspanningen van vele vrijwilligers, stilletjes weer rustiger
+                wateren op te zoeken. Vanuit een nieuwe structuur wordt nu
+                gewerkt aan een gezonde club, waarin de jeugopleiding, het
+                doorstromen van de jeugd naar de eerste ploeg, en het inzetten
+                van voornamelijk spelers uit de nabije omgeving, de
+                belangrijkste zaken zijn.
+              </p>
+            </div>
+          </div>
+
+          <div class="timeline-item">
+            <div class="timeline-icon"></div>
+            <div class="timeline-content right">
+              <p class="timeline-content-date">2018 - ...</p>
+              <p>
+                In het seizoen 2018/2019 werd KCVV Elewijt kampioen met maar
+                liefst 79 punten, na een nek-aan-nek race tot de allerlaatste
+                speeldag (slechts één punt voorsprong op de tweede in de stand
+                Mazenzele-Opwijk).
+              </p>
+              <p>
+                De ploeg zal in het seizoen 2019/2020 ingedeeld zijn in tweede
+                provinciale A (Brabant).
+              </p>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default HistoryPage
---
+++ src/pages/club/responsibilities.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import Layout from '../../layouts/index'
+
+import SEO from '../../components/seo'
+
+class ResponsibilitiesPage extends Component {
+  render() {
+    return (
+      <Layout>
+        <SEO lang="nl-BE" title="Er is maar één plezante compagnie" />
+
+        KOMT ERAAN
+      </Layout>
+    )
+  }
+}
+
+export default ResponsibilitiesPage
