14edfe3e0fd2a84d0465eb478eef5d200fee081b
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2019-08-29 21:19:43 +0200

    Search-JS.

---
+++ src/pages/search.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+import Layout from '../layouts/index'
+
+import SEO from '../components/seo'
+import Search from '../components/search-container';
+
+class SearchPage extends Component {
+  render() {
+    return (
+      <Layout>
+        <SEO lang="nl-BE" title="Zoeken | Er is maar één plezante compagnie" />
+
+        <div className={'limited-width_wrapper'}>
+          <h1>Zoeken</h1>
+
+          <Search />
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default SearchPage
