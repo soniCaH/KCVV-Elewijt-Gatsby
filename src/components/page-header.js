1bdb8b60c78af83c0220933500a8db48bc02319a
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2019-10-04 18:31:03 +0200

    Search icon in menu.

--- src/components/page-header.js
+++ src/components/page-header.js
@@ -307,6 +307,11 @@ class MenuItems extends Component {
             </li>
           </ul>
         </li>
+        <li>
+          <Link to="/search/" activeClassName="active" partiallyActive={true}>
+            <i className={"fa fa-search"} aria-hidden="true"></i>
+          </Link>
+        </li>
       </Fragment>
     )
   }
