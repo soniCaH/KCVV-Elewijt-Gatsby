8b203ece4db42edecfa520424b83d25985e370e9
Kevin Van Ransbeeck <climacon@gmail.com>
2019-11-30 10:05:11 +0100

    Error 404 page styling.

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,6 +4,7 @@ import Layout from '../layouts/index'
 
 import SEO from '../components/seo'
 import HeaderBgTitle from '../components/header-bg-title'
+import { Link } from 'gatsby'
 
 class PageNotFoundPage extends Component {
   render() {
@@ -11,7 +12,39 @@ class PageNotFoundPage extends Component {
       <Layout>
         <SEO lang="nl-BE" title="Er is maar één plezante compagnie" />
 
-        <HeaderBgTitle />
+        <HeaderBgTitle title="Pagina" highlight="niet gevonden" classes={'error-page--404 error-page'} />
+
+        <div className={'grid-container site-content'}>
+          <div className={'grid-x grid-margin-x'}>
+            <div className={'cell large-12 center'}>
+              <h2>Oeps!</h2>
+
+              <h3>De pagina die u zocht bestaat niet (meer)</h3>
+
+              <p>De opgevraagde pagina werd niet teruggevonden. </p>
+              <p>
+                Als u op deze pagina terecht bent gekomen door ergens op een
+                link te klikken, <br />
+                mag je ons dat zeker laten weten via{' '}
+                <a href="mailto:website@kcvvelewijt.be">
+                  {' '}
+                  website@kcvvelewijt.be{' '}
+                </a>
+                <br />
+                dan kunnen we dit nakijken en hopelijk oplossen!
+              </p>
+              <h3>Wegwijs</h3>
+              <p>
+                <Link to={'/search'} className={'btn btn--arrow'}>
+                  Ga terug naar start
+                </Link>
+                <Link to={'/search'} className={'btn btn--alt btn--arrow'}>
+                  Ga naar de zoekpagina
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
       </Layout>
     )
   }
