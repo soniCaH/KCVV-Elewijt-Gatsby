7f137a2437cd335313e0891a12c773f1ff0c1457
Kevin Van Ransbeeck <climacon@gmail.com>
2019-11-30 09:13:30 +0100

    Updates to settings.

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import Layout from '../layouts/index'
 
 import SEO from '../components/seo'
+import HeaderBgTitle from '../components/header-bg-title'
 
 class PageNotFoundPage extends Component {
   render() {
@@ -10,10 +11,7 @@ class PageNotFoundPage extends Component {
       <Layout>
         <SEO lang="nl-BE" title="Er is maar één plezante compagnie" />
 
-        <article>
-          Deze site is nog volop in opbouw. <br />
-          De pagina die u zocht is wellicht nog niet ontwikkeld.
-        </article>
+        <HeaderBgTitle />
       </Layout>
     )
   }
