ca23f4c84e3d259fc205ccdaa3ffb451eb4f584a
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2020-07-03 21:49:20 +0200

    Update pre-season B-team friendlies.

--- src/pages/games.js
+++ src/pages/games.js
@@ -189,6 +189,71 @@ class GamesPage extends Component {
       regNumberHome: "07941",
       regNumberAway: "00055",
     }
+    const wedstrijd16 = {
+      region: "bra",
+      division: "FR",
+      dateTime: 1596562200000,
+      home: "SK Heffen",
+      away: "KCVV Elewijt B",
+      resultHome: "",
+      resultAway: "",
+      status: "",
+      // matchDay,
+      regNumberHome: "03738",
+      regNumberAway: "00055",
+    }
+    const wedstrijd17 = {
+      region: "bra",
+      division: "FR",
+      dateTime: 1596736800000,
+      home: "SK Laar",
+      away: "KCVV Elewijt B",
+      resultHome: "",
+      resultAway: "",
+      status: "",
+      // matchDay,
+      regNumberHome: "R61054",
+      regNumberAway: "00055",
+    }
+    const wedstrijd18 = {
+      region: "bra",
+      division: "FR",
+      dateTime: 1597080600000,
+      home: "KCVV Elewijt B",
+      away: "KFC Katelijne-Waver B",
+      resultHome: "",
+      resultAway: "",
+      status: "",
+      // matchDay,
+      regNumberHome: "00055",
+      regNumberAway: "04453",
+    }
+    const wedstrijd19 = {
+      region: "bra",
+      division: "FR",
+      dateTime: 1597685400000,
+      home: "KCVV Elewijt B",
+      away: "KFC Malderen B",
+      resultHome: "",
+      resultAway: "",
+      status: "",
+      // matchDay,
+      regNumberHome: "00055",
+      regNumberAway: "03794",
+    }
+    const wedstrijd20 = {
+      region: "bra",
+      division: "FR",
+      dateTime: 1598464800000,
+      home: "KCVV Elewijt B",
+      away: "FC St Jozef Londerzeel",
+      resultHome: "",
+      resultAway: "",
+      status: "",
+      // matchDay,
+      regNumberHome: "00055",
+      regNumberAway: "03959",
+    }
     return (
       <Layout>
         <SEO
@@ -199,17 +264,22 @@ class GamesPage extends Component {
         />
         <div className={"games__template"}>
           <MatchWithLogo match={wedstrijd1} />
+          <MatchWithLogo match={wedstrijd16} />
           <MatchWithLogo match={wedstrijd2} />
           <MatchWithLogo match={wedstrijd3} />
+          <MatchWithLogo match={wedstrijd17} />
           <MatchWithLogo match={wedstrijd4} />
           <MatchWithLogo match={wedstrijd5} />
+          <MatchWithLogo match={wedstrijd18} />
           <MatchWithLogo match={wedstrijd6} />
           <MatchWithLogo match={wedstrijd7} />
           <MatchWithLogo match={wedstrijd9} />
           <MatchWithLogo match={wedstrijd10} />
+          <MatchWithLogo match={wedstrijd19} />
           <MatchWithLogo match={wedstrijd11} />
           <MatchWithLogo match={wedstrijd12} />
           <MatchWithLogo match={wedstrijd13} />
+          <MatchWithLogo match={wedstrijd20} />
           <MatchWithLogo match={wedstrijd14} />
           <MatchWithLogo match={wedstrijd15} />
         </div>
