7e614a8f68557b28f50acabb5238eba2ae25b250
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2020-08-22 10:02:35 +0200

    Facebook fa-icon in teasers.

--- src/components/cards.js
+++ src/components/cards.js
@@ -11,6 +11,7 @@ export class Card extends Component {
   render() {
     const {
       title,
+      icon = null,
       body = null,
       localFile,
       link,
@@ -41,7 +42,7 @@ export class Card extends Component {
 
             <main className={"cardItem__summary"}>
               <div className={"cardItem__heading"}>
-                <h3>{title}</h3>
+                <h3>{icon && <i className={`fa ${icon}`} aria-hidden={true}></i>} {title}</h3>
               </div>
 
               {body && <div dangerouslySetInnerHTML={{ __html: body }}></div>}
@@ -57,7 +58,7 @@ export class Card extends Component {
 
             <main className={"cardItem__summary"}>
               <div className={"cardItem__heading"}>
-                <h3>{title}</h3>
+                <h3>{icon && <i className={`fa ${icon}`} aria-hidden={true}></i>} {title}</h3>
               </div>
 
               {body && <div dangerouslySetInnerHTML={{ __html: body }}></div>}
