3a951d0e59253bdc796c462c2765682548a9fd2e
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2021-06-10 11:02:25 +0200

    Try without some caching.

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
   flags: {
     FAST_DEV: true,
     PRESERVE_WEBPACK_CACHE: true,
-    PRESERVE_FILE_DOWNLOAD_CACHE: true,
-    PARALLEL_SOURCING: true,
+    // PRESERVE_FILE_DOWNLOAD_CACHE: true,
+    // PARALLEL_SOURCING: true,
   },
 }
