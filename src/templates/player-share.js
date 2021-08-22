d452454b936e801eb13b0201ff026ebf12aa7a2a
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2021-08-22 12:02:34 +0200

    Test new styling.

--- src/templates/player-share.js
+++ src/templates/player-share.js
@@ -14,45 +14,15 @@ export const query = graphql`
       path {
         alias
       }
-      body {
-        processed
-      }
       title
-      field_join_date
-      field_date_leave
       field_lastname
-      field_position
       field_firstname
-      field_birth_date
       field_shirtnumber
       field_vv_id
       relationships {
-        node__article {
-          title
-          timestamp: created(formatString: "x")
-          path {
-            alias
-          }
-          relationships {
-            field_media_article_image {
-              ...ArticleImage
-            }
-          }
-        }
-        node__team {
-          title
-          relationships {
-            field_media_article_image {
-              ...ArticleImage
-            }
-          }
-          path {
-            alias
-          }
-        }
         field_image {
           localFile {
-            ...KCVVFixedPlayerTeaser
+            ...KCVVFixedPlayerTeaserShare
           }
         }
       }
