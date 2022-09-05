3a48de362d427819430857cdba849785c9873fa4
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2022-09-05 13:28:51 +0200

    Only deploy published content.

--- src/Gatsby/PageQueries.tsx
+++ src/Gatsby/PageQueries.tsx
@@ -1,93 +1,91 @@
 export const gatsbyNodePageQueries = `
-articles: allNodeArticle(sort: {fields: created, order: DESC}) {
+articles: allNodeArticle(
+    sort: {fields: created, order: DESC}
+    filter: {status: {eq: true}}
+  ) {
     edges {
-        node {
-            title
-            created(formatString: "DD/MM/YYYY")
-            path {
-                alias
-            }
+      node {
+        title
+        created(formatString: "DD/MM/YYYY")
+        path {
+          alias
+        }
+        relationships {
+          field_media_article_image {
             relationships {
-                field_media_article_image {
-                    relationships {
-                        field_media_image {
-                            localFile {
-                                childImageSharp {
-                                    gatsbyImageData(
-                                        width: 960
-                                        placeholder: DOMINANT_COLOR
-                                        layout: CONSTRAINED
-                                        aspectRatio: 1.5
-                                        transformOptions: { cropFocus: ATTENTION }
-                                    )
-                                }
-                            }
-                        }
-                    }
+              field_media_image {
+                localFile {
+                  childImageSharp {
+                    gatsbyImageData(
+                      width: 960
+                      placeholder: DOMINANT_COLOR
+                      layout: CONSTRAINED
+                      aspectRatio: 1.5
+                      transformOptions: {cropFocus: ATTENTION}
+                    )
+                  }
                 }
-                field_tags {
-                    name
-                    path {
-                        alias
-                    }
-                }
+              }
             }
-        }
-    }
-}
-
-pages: allNodePage {
-    edges {
-        node {
-            title
-            path {
-                alias
-            }
-        }
-    }
-}
-
-teams: allNodeTeam {
-    edges {
-        node {
-            title
-            path {
-                alias
-            }
-        }
-    }
-}
-
-players: allNodePlayer {
-    edges {
-        node {
-            title
-            path {
-                alias
-            }
-        }
-    }
-}
-
-staff: allNodeStaff {
-    edges {
-        node {
-            title
-            path {
-                alias
-            }
-        }
-    }
-}
-
-categories: allTaxonomyTermCategory {
-    edges {
-        node {
+          }
+          field_tags {
             name
             path {
-                alias
+              alias
             }
+          }
         }
+      }
     }
-}
+  }
+  pages: allNodePage(filter: {status: {eq: true}}) {
+    edges {
+      node {
+        title
+        path {
+          alias
+        }
+      }
+    }
+  }
+  teams: allNodeTeam(filter: {status: {eq: true}}) {
+    edges {
+      node {
+        title
+        path {
+          alias
+        }
+      }
+    }
+  }
+  players: allNodePlayer(filter: {status: {eq: true}}) {
+    edges {
+      node {
+        title
+        path {
+          alias
+        }
+      }
+    }
+  }
+  staff: allNodeStaff(filter: {status: {eq: true}}) {
+    edges {
+      node {
+        title
+        path {
+          alias
+        }
+      }
+    }
+  }
+  categories: allTaxonomyTermCategory(filter: {status: {eq: true}}) {
+    edges {
+      node {
+        name
+        path {
+          alias
+        }
+      }
+    }
+  }
 `
